9eed3ffa4d8cf09d20ddf33da9273302d2a88276
Moffaty <goga459972@gmail.com>
2024-08-30 09:18:52 +0300

    init

---
+++ db.js
@@ -0,0 +1,32 @@
+import { Sequelize, DataTypes } from 'sequelize'
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'marks.sqlite',
+})
+
+const User = sequelize.define(
+    'Mark',
+    {
+        Name: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        // Model attributes are defined here
+        x: {
+            type: DataTypes.NUMBER,
+            allowNull: false,
+        },
+        y: {
+            type: DataTypes.NUMBER,
+            allowNull: false,
+            // allowNull defaults to true
+        },
+    },
+    {
+        // Other model options go here
+    }
+)
+
+// `sequelize.define` also returns the model
+sequelize.sync({ force: true }) // create table
